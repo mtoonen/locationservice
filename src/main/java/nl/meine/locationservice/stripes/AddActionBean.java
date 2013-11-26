bccad224c3456413297d4e03dbfcbe7a5a576683
Meine Toonen <toonen.meine@gmail.com>
2013-11-26 22:33:50 +0100

    Save first try

--- src/main/java/nl/meine/locationservice/stripes/AddActionBean.java
+++ src/main/java/nl/meine/locationservice/stripes/AddActionBean.java
@@ -4,6 +4,13 @@
  */
 package nl.meine.locationservice.stripes;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.sql.DataSource;
 import net.sourceforge.stripes.action.ActionBean;
 import net.sourceforge.stripes.action.ActionBeanContext;
 import net.sourceforge.stripes.action.DefaultHandler;
@@ -12,27 +19,26 @@ import net.sourceforge.stripes.action.Resolution;
 import net.sourceforge.stripes.action.StrictBinding;
 import net.sourceforge.stripes.action.UrlBinding;
 import net.sourceforge.stripes.validation.Validate;
+import org.apache.commons.dbutils.DbUtils;
+import org.apache.commons.dbutils.QueryRunner;
 
 /**
  *
  * @author Meine Toonen <toonen.meine@gmail.com>
  */
-
-@UrlBinding("/add/{id}/{x},{y}")
+@UrlBinding("/add/{key}/{x},{y}")
 @StrictBinding
-public class AddActionBean implements ActionBean{
+public class AddActionBean implements ActionBean {
+
     private ActionBeanContext context;
-    private static final String JSP  = "/WEB-INF/jsp/view.jsp";
-    
+    private static final String JSP = "/WEB-INF/jsp/view.jsp";
     @Validate
-    private Integer id;
-    
+    private String key;
     @Validate
     private Integer x;
-    
     @Validate
     private Integer y;
-    
+
     // <editor-fold desc="Getters and Setters" defaultstate="collapsed" >
     @Override
     public void setContext(ActionBeanContext abc) {
@@ -44,12 +50,12 @@ public class AddActionBean implements ActionBean{
         return this.context;
     }
 
-    public Integer getId() {
-        return id;
+    public String getKey() {
+        return key;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
+    public void setKey(String key) {
+        this.key = key;
     }
 
     public Integer getX() {
@@ -68,14 +74,34 @@ public class AddActionBean implements ActionBean{
         this.y = y;
     }
     // </editor-fold>
-    
-    
+
     @DefaultHandler
-    public Resolution view(){
+    public Resolution view() {
+        save();
         return new ForwardResolution(JSP);
     }
-    
-    private void save(){
-        
+
+    private void save() {
+        Connection conn= null;
+        try {
+            conn = getConnection();
+
+    // No DataSource so we must handle Connections manually
+            QueryRunner run = new QueryRunner();
+            run.update(conn,"INSERT INTO location (key,x,y) VALUES (?,?,?)",key,x,y);
+            int a = 0;
+        } catch (SQLException ex) {
+            Logger.getLogger(AddActionBean.class.getName()).log(Level.SEVERE, null, ex);
+        }finally{
+            DbUtils.closeQuietly(conn);
+            
+        }
+    }
+
+    private Connection getConnection() throws SQLException {
+        Properties props = new Properties();
+        props.put("user", "locationservice");
+        props.put("password", "locationservice  ");
+        return DriverManager.getConnection("jdbc:postgresql://localhost/locationservice", props);
     }
 }
